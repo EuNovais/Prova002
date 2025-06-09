4175d8ddc897df74d1ebb980b24077825bd73288
EuNovais <victor.costa@sou.unifeb.edu.br>
2025-06-09 13:05:13 -0300

    classe cliente c/ atributos

--- Prova002/Cliente.cs
+++ Prova002/Cliente.cs
@@ -8,9 +8,17 @@ namespace Prova002
 {
     public class Cliente
     {
-        public int Id { get; set;}
+        public int Id { get;}
         public string Nome { get;}
         public string Email { get;}
         public string Cpf { get;}
+
+        public Cliente(int id, string nome, string email, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+        }
     }
 }
