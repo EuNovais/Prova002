36c6a15293b4a4593847c2988d43307511ff8018
euvictornovais <arrotogamer390@gmail.com>
2025-06-09 12:52:46 -0300

    Adicionar arquivos de projeto.

---
+++ Prova002/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova002
+{
+    public class Produto
+    {
+        public int Id { get; }
+        public string Nome { get; }
+        public decimal Preco { get; }
+        public string Categoria { get; }
+
+        public Produto(int id, string nome, decimal preco, string categoria)
+        {
+            if (preco <= 0)
+                throw new ArgumentException("Preço invalido.");
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Categoria = categoria;
+        }
+    }
+}
