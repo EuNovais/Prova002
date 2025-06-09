238629020d469964dfe4136f92457085c1ba7d27
EuNovais <victor.costa@sou.unifeb.edu.br>
2025-06-09 15:17:05 -0300

    classe de pedido,conceito de solid, responsabilidade unica e aberto para extenasao

---
+++ Prova002/Pedido.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova002
+{
+    public class Pedido
+    {
+        public int Id { get; }                
+        public Cliente Cliente { get; }         
+        public List<ItemPedido> Itens { get; }     
+        public DateTime Data { get; }                
+        public decimal ValorTotal => Itens.Sum(i => i.Subtotal);
+
+       
+        public Pedido(int id, Cliente cliente, List<ItemPedido> itens)
+        {
+            Id = id;                  
+            Cliente = cliente;         
+            Itens = itens;             
+            Data = DateTime.Now;       
+        }
+    }
+}
