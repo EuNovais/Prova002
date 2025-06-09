502ea90bac2dabd8b1bbf097972ac6feafa8fe08
EuNovais <victor.costa@sou.unifeb.edu.br>
2025-06-09 17:05:35 -0300

    Classe responsável por criar objetos Pedido (padrão Factory Method) possuindo o D e o O do conceito SOLID. O - Princípio Aberto/Fechado: possível criar outras fábricas sem modificar essa  D - Inversão de Dependência: evita acoplamento direto ao construtor de Pedido

---
+++ Prova002/PedidoFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova002
+{
+    public class PedidoFactory
+    {
+        
+        public Pedido CriarPedido(int id, Cliente cliente, List<ItemPedido> itens)
+        {
+            return new Pedido(id, cliente, itens); 
+        }
+    }
+}
