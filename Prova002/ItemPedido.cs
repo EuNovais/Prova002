b347f4e5ac306009f31b2068cbc19dd57548abde
EuNovais <victor.costa@sou.unifeb.edu.br>
2025-06-09 14:25:52 -0300

    classe item pedidos, com o S do solid aplicado, responsabilidade unicas, nas outras classes acontecem o "s" do solid, mas esqueci de comentar

---
+++ Prova002/ItemPedido.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova002
+{
+    public class ItemPedido
+    {
+        public Produto Produto { get; }      
+        public int Quantidade { get; }       
+
+        
+        public ItemPedido(Produto produto, int quantidade)
+        {
+            Produto = produto;               
+            Quantidade = quantidade;         
+        }
+
+        public decimal Subtotal => Produto.Preco * Quantidade;
+    }
+}
