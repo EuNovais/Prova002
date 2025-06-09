4a20db980d61af0f28bdc0a9a888165544445ac8
EuNovais <victor.costa@sou.unifeb.edu.br>
2025-06-09 13:01:51 -0300

    testando commit git

---
+++ Prova002/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova002
+{
+    public class Cliente
+    {
+        public int Id { get; set;}
+        public string Nome { get;}
+        public string Email { get;}
+        public string Cpf { get;}
+    }
+}
