63d4e56592e97c9ab3ec647462a9dcb1e6c5cd0f
EuNovais <victor.costa@sou.unifeb.edu.br>
2025-06-09 22:31:40 -0300

    implementa pedidoservice com aplicação de descontos via Strategy e injeção de dependência, e usa o S do conceito SOLId

---
+++ Prova002/ILogService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova002
+{ 
+    //interface para a log
+    public interface ILogService
+    {
+        void Registrar(string mensagem); 
+    }
+}
---
+++ Prova002/PedidoService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova002
+{
+    public class PedidoService
+    {
+        private readonly IDescontoStrategy _descontoStrategy; 
+        private readonly ILogService _log;                    
+
+        // Injeção de dependências via construtor
+        public PedidoService(IDescontoStrategy descontoStrategy, ILogService log)
+        {
+            _descontoStrategy = descontoStrategy; 
+            _log = log;                           
+        }
+
+        
+        public decimal CalcularTotalComDesconto(Pedido pedido)
+        {
+            decimal desconto = _descontoStrategy.CalcularDesconto(pedido); 
+            _log.Registrar($"Desconto aplicado: {desconto:C}");             
+            return pedido.ValorTotal - desconto;                            
+        }
+    }
+}
